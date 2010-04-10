9dd68e23ccaf548bd51ca2e902f0531ba0eac66b
hgarcia <hernan@dynamicprogrammer.com>
2010-04-09 23:20:06 -0400

    Adding a demo for StoryQ

---
+++ Talks/BDD/StoryQ/Stories/AddNumbers.cs
@@ -0,0 +1,50 @@
+﻿using System.Reflection;
+using Calculator;
+using NUnit.Framework;
+using StoryQ;
+
+namespace Stories
+{
+    [TestFixture]
+    public class AddNumbers
+    {
+        private Basic _calculator;
+
+        [Test]
+        public void AddingNumbers()
+        {
+            new Story("Adding two numbers")
+                .InOrderTo("don't make mistakes")
+                .AsA("accountant")
+                .IWant("to use a calculator")
+                .WithScenario("Add two positive numbers")
+                .Given(IStartTheCalculator)
+                .And(EnterNumber,25)
+                .And(EnterNumber,25)
+                .When(CallAdd)
+                .Then(ResultShouldBe,50)
+                .ExecuteWithReport(MethodBase.GetCurrentMethod());
+        }
+
+        private void ResultShouldBe(int result)
+        {
+            Assert.That(result,Is.EqualTo(_calculator.GetResult()));
+        }
+
+        private void EnterNumber(int number)
+        {
+            _calculator.Enter(number);
+        }
+
+        private void IStartTheCalculator()
+        {
+            _calculator = new Basic();
+        }
+
+        private void CallAdd()
+        {
+            _calculator.Add();
+        }
+
+    }
+}
